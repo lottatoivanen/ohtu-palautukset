1887264d1740f600c763174de3fb504d6c761dc5
Lotta Toivanen <lotta.toivanen@helsinki.fi>
2025-11-07 13:09:40 +0200

    Modify python files to make index.py work the same as before

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,19 +1,16 @@
-import requests
-from player import Player
+from playerreader import PlayerReader
+from playerstats import PlayerStats
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    players = [Player(player_dict) for player_dict in response]
-    psorted = sorted(players, key=lambda p: p.assists + p.goals, reverse=True)
-    nationality = "FIN"
-    chosen = [p for p in psorted if p.nationality == nationality]
-
-    print(f"Players from {nationality}:")
-
-    for player in chosen:
+    for player in players:
         print(player)
+
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/playerstats.py
+++ viikko2/nhl-reader/src/playerstats.py
@@ -1,2 +1,10 @@
 class PlayerStats:
-    
+    def __init__(self, reader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nationality):
+        players = self.reader.get_players()
+        psorted = sorted(players, key=lambda p: p.assists + p.goals, reverse=True)
+        chosen = [p for p in psorted if p.nationality == nationality]
+        return chosen
+        
