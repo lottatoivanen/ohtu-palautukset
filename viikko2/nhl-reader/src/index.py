d13659711b40ad8eceb61467e9615168f6406efb
Lotta Toivanen <lotta.toivanen@helsinki.fi>
2025-11-07 12:35:55 +0200

    Modified index.py and player.py to sort playerlist by sum

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -6,8 +6,9 @@ def main():
     response = requests.get(url).json()
 
     players = [Player(player_dict) for player_dict in response]
+    psorted = sorted(players, key=lambda p: p.assists + p.goals, reverse=True)
     nationality = "FIN"
-    chosen = [p for p in players if p.nationality == nationality]
+    chosen = [p for p in psorted if p.nationality == nationality]
 
     print(f"Players from {nationality}:")
 
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -2,11 +2,12 @@ class Player:
     def __init__(self, dict):
         self.name = dict['name']
         self.nationality = dict['nationality']
-        self.assits = dict['assists']
+        self.assists = dict['assists']
         self.goals = dict['goals']
         self.team = dict['team']
         self.games = dict['games']
         self.id = dict['id']
     
     def __str__(self):
-        return f"{self.name} team {self.team} goals {self.goals} assists {self.assits}"
+        summa = self.assists + self.goals
+        return f"{self.name:20} {self.team:15} {self.goals:2} + {self.assists:2} = {summa:2}"
